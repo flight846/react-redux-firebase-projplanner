6e21aa9282d6dd49a1cdeffa8e51cf158fbbebdd
yazid <yazid@decision-science.com>
2019-02-09 18:04:06 +0800

    Add project dummy data state

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,11 @@
-const initState = {}
+const initState = {
+    projects: [
+        { id: '1', title: 'Help me find peach', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+        { id: '2', title: 'Collect all the pokemons', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+        { id: '3', title: 'Egg hunt with Michael', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    ]
+}
+
 const projectReducer = (state = initState, action) => {
     return state;
 }
